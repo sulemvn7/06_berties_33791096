e79406d75e845c9c62247866dee0c28f0deacf06
Suleman <149071013+sulemvn7@users.noreply.github.com>
2025-11-19 11:25:12 +0000

    Add files via upload

---
+++ index.js
@@ -0,0 +1,53 @@
+require('dotenv').config(); // Load environment variables from .env
+
+// Import express and ejs
+var express = require ('express')
+var ejs = require('ejs')
+const path = require('path')
+var mysql = require('mysql2');
+
+// Create the express application object
+const app = express()
+const port = 8000
+
+// Tell Express that we want to use EJS as the templating engine
+app.set('view engine', 'ejs')
+
+// Set up the body parser 
+app.use(express.urlencoded({ extended: true }))
+
+// Set up public folder (for css and static js)
+app.use(express.static(path.join(__dirname, 'public')))
+
+// Define our application-specific data
+app.locals.shopData = {shopName: "Bertie's Books"}
+
+// Define the database connection pool
+const db = mysql.createPool({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0,
+});
+global.db = db;
+
+
+// Load the route handlers
+const mainRoutes = require("./routes/main")
+app.use('/', mainRoutes)
+
+// Load the route handlers for /users
+const usersRoutes = require('./routes/users')
+app.use('/users', usersRoutes)
+
+// Load the route handlers for /books
+const booksRoutes = require('./routes/books')
+app.use('/books', booksRoutes)
+
+// Start the web app listening
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+
+
